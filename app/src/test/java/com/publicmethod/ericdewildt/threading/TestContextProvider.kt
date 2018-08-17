7a7ede2d115c0f6e34547c6262728157777af5b4
Eric De Wildt <dewildte@gmail.com>
2018-08-17 13:34:27 -0400

    Re-theme the BottomAppBar Overflow menu

--- app/src/test/java/com/publicmethod/ericdewildt/threading/TestContextProvider.kt
+++ app/src/test/java/com/publicmethod/ericdewildt/threading/TestContextProvider.kt
@@ -1,9 +1,9 @@
 package com.publicmethod.ericdewildt.threading
 
 import kotlinx.coroutines.experimental.Unconfined
+import kotlinx.coroutines.experimental.android.HandlerContext
 import kotlin.coroutines.experimental.CoroutineContext
 
 open class TestContextProvider : ContextProvider() {
-    override fun uiContext(): CoroutineContext = Unconfined
     override fun backgroundContext(): CoroutineContext = Unconfined
 }
